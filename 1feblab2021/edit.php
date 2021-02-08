b7d5bae51d8f183d63f6e23a4ebc166d67e626dc
Architgarg1105 <architgarglmg9.4@gmail.com>
2021-02-08 11:33:26 +0530

    update radio and select tag

--- 1feblab2021/edit.php
+++ 1feblab2021/edit.php
@@ -2,8 +2,6 @@
 include('config.php');
 
 ?>
-
-
 <?php
 $id=$_GET['id'];
 $fetch1 = mysqli_query($conn,"select * from users where id = $id");
@@ -12,6 +10,8 @@ $fetch2 = mysqli_query($conn,"select * from student_details where username = $id
     $var2 = mysqli_fetch_array($fetch2);
 $val1= $var1['username'];
 $val2= $var1['email'];
+$val3= $var1['gender'];
+$val4= $var1['city'];
 $val5= $var2['branch'];
 $val6= $var2['year'];
 if(isset($_POST['submit']))
@@ -30,8 +30,9 @@ if(isset($_POST['submit']))
     $sql= "UPDATE users SET username = '$username', email = '$email', gender='$gender', city='$city' WHERE id = $id" ;
     mysqli_query($conn,$sql);
     $sql1= "UPDATE student_details SET branch = '$branch', year = '$year' WHERE username = $id" ;
-    mysqli_query($conn,$sql1);
-    echo "Updated";
+    if(mysqli_query($conn, $sql1)){
+        header("Location:details.php");
+    }
 }
 else{
     echo "Please click submit button to submit the data..";
@@ -45,28 +46,28 @@ else{
         <title>HTML Forms</title>
     </head>
 <body>
-<form method="POST">
+<form method="POST" action="edit.php?id=<?php echo "$id"?>">
     USERNAME <input type="text" name="username" value="<?php echo "$val1" ?>" placeholder="Type Your Username" required><br>
     E-MAIL <input type="email" name="email" value="<?php echo "$val2" ?>" placeholder="Type Your E-mail" required><br>
-    GENDER<br> <input type="radio" id="male" name="gender" value="male" checked="checked">
+    GENDER<br> <input type="radio" id="male" name="gender" value="male" <?php if($val3=="male"){echo "checked";}?>>
     <label for="male">Male</label><br>
-    <input type="radio" id="female" name="gender" value="female">
+    <input type="radio" id="female" name="gender" value="female" <?php if($val3=="female"){echo "checked";}?>>
     <label for="female">Female</label><br>
-    <input type="radio" id="other" name="gender" value="other">
+    <input type="radio" id="other" name="gender" value="other" <?php if($val3=="other"){echo "checked";}?>>
     <label for="other">Other</label>
     <br>
     Select City <select name="city">
-        <option value="Dehradun">Dehradun</option>
-        <option value="Delhi">Delhi</option>
-        <option value="Jaipur">Jaipur</option>
-        <option value="Udaipur">Udaipur</option>
-        <option value="Kota">Kota</option>
-        <option value="Shimla">Shimla</option>
-        <option value="Noida">Noida</option>
-        <option value="Srinagar">Srinagar</option>
-        <option value="Ambala">Ambala</option>
-        <option value="Uana">Uana</option>
-        <option value="Alwar">Alwar</option>
+        <option value="Dehradun" <?php if($val4=="Dehradun"){echo "selected";}?>>Dehradun</option>
+        <option value="Delhi" <?php if($val4=="Delhi"){echo "selected";}?>>Delhi</option>
+        <option value="Jaipur" <?php if($val4=="Jaipur"){echo "selected";}?>>Jaipur</option>
+        <option value="Udaipur" <?php if($val4=="Udaipur"){echo "selected";}?>>Udaipur</option>
+        <option value="Kota" <?php if($val4=="Kota"){echo "selected";}?>>Kota</option>
+        <option value="Shimla" <?php if($val4=="Shimla"){echo "selected";}?>>Shimla</option>
+        <option value="Noida" <?php if($val4=="Noida"){echo "selected";}?>>Noida</option>
+        <option value="Srinagar" <?php if($val4=="Srinagar"){echo "selected";}?>>Srinagar</option>
+        <option value="Ambala" <?php if($val4=="Ambala"){echo "selected";}?>>Ambala</option>
+        <option value="Uana" <?php if($val4=="Uana"){echo "selected";}?>>Uana</option>
+        <option value="Alwar" <?php if($val4=="Alwar"){echo "selected";}?>>Alwar</option>
     </select><br>
     BRANCH<input type="text" name="branch" value="<?php echo "$val5" ?>" placeholder="Enter Branch" required><br>
     YEAR<input type="text" name="year" value="<?php echo "$val6" ?>" placeholder="Enter Year" required><br>
